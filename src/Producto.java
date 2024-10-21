2daf311bb14e329eeef53c38381e4be84edf6af6
Giovan Herrera <gio2020herrera@gmail.com>
2024-10-21 11:47:21 -0500

    creacion de getters and setters de producto

--- src/Producto.java
+++ src/Producto.java
@@ -4,12 +4,52 @@ public class Producto {
     private String numSerie;
     private double valor;
     private String nombreProveedor;
-    
+
     public Producto(int id, String nombre, String numSerie, double valor, String nombreProveedor) {
         this.id = id;
         this.nombre = nombre;
         this.numSerie = numSerie;
         this.valor = valor;
+        this.nombreProveedor = nombreProveedor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumSerie() {
+        return numSerie;
+    }
+
+    public void setNumSerie(String numSerie) {
+        this.numSerie = numSerie;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getNombreProveedor() {
+        return nombreProveedor;
+    }
+
+    public void setNombreProveedor(String nombreProveedor) {
         this.nombreProveedor = nombreProveedor;
     }
 }
