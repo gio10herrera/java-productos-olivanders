8c7e96272d59e961c6ffd11f4a6b28f4395ae523
Giovan Herrera <gio2020herrera@gmail.com>
2024-10-24 16:14:54 -0500

    creando metodo modificar producto, hace falta modificar el numero de existencias dentro del metodo

--- src/ComercializadoraOlivanders.java
+++ src/ComercializadoraOlivanders.java
@@ -12,8 +12,7 @@ public class ComercializadoraOlivanders {
     static boolean salir = false;
 
     public static void main(String[] args) {
-        int opcion, existencias;
-        double valor;
+        int opcion;
         opcion = menu();
         while (!salir) {
             switch (opcion) {
@@ -21,19 +20,61 @@ public class ComercializadoraOlivanders {
                     registrarProducto();
                     opcion = menu();
                 }
-                case 2 -> /*modificarProducto();*/salir = true;
+                case 2 -> {
+                    modificarProducto();
+                    opcion = menu();
+                }
                 case 3 -> /*eliminarProducto();*/salir = true;
-                case 5 -> {
+                case 4 -> {
                     listarProductos();
                     opcion = menu();
                 }
-                case 6 -> salir = true;
+                case 5 -> salir = true;
+                default -> JOptionPane.showMessageDialog(null, "Opcion no valida", "Error", JOptionPane.ERROR_MESSAGE);
             }
         }
     }
 
+    private static void modificarProducto() {
+        int id = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite el id del producto", "Buscar producto", JOptionPane.INFORMATION_MESSAGE));
+        Producto p = getProductoById(id);
+        if (p != null) {
+            int op = Integer.parseInt(JOptionPane.showInputDialog(null, "1. Modificar nombre del producto\n2. Modificar precio del producto\n3. Modificar nombre del proveedor", "Modificar producto", JOptionPane.INFORMATION_MESSAGE));
+
+            switch (op) {
+                case 1 -> {
+                    String name = JOptionPane.showInputDialog(null, "Digite el nuevo nombre del producto", "Modificar nombre", JOptionPane.INFORMATION_MESSAGE);
+                    p.setNombre(name);
+                    JOptionPane.showMessageDialog(null, "Nombre modificado", "Exitoso", JOptionPane.INFORMATION_MESSAGE);
+                }
+                case 2 -> {
+                    double price = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite el precio del producto", "Modificar precio", JOptionPane.INFORMATION_MESSAGE));
+                    p.setValor(price);
+                    JOptionPane.showMessageDialog(null, "Precio modificado", "Exitoso", JOptionPane.INFORMATION_MESSAGE);
+                }
+                case 3 -> {
+                    String providerName = JOptionPane.showInputDialog(null, "Digite el nuevo nombre del producto", "Modificar nombre", JOptionPane.INFORMATION_MESSAGE);
+                    p.setNombreProveedor(providerName);
+                    JOptionPane.showMessageDialog(null, "Nombre del proveedor modificado", "Exitoso", JOptionPane.INFORMATION_MESSAGE);
+                }
+                default -> JOptionPane.showMessageDialog(null, "Opcion no valida", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "No existe el producto", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private static Producto getProductoById(int id) {
+        for (Producto p : productos) {
+            if (p.getId() == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+
     private static int menu() {
-        return Integer.parseInt(JOptionPane.showInputDialog(null, "Menu: \n1. Registrar producto\n2. Modificar producto \n3. Eliminar producto\4. Venta\n5. Mostrar productos\n6. Salir", "Numero de productos", JOptionPane.INFORMATION_MESSAGE));
+        return Integer.parseInt(JOptionPane.showInputDialog(null, "Menu: \n1. Registrar producto\n2. Modificar producto \n3. Eliminar producto\4. Venta\n4. Mostrar productos\n5. Salir", "Numero de productos", JOptionPane.INFORMATION_MESSAGE));
     }
 
     private static void registrarProducto() {
